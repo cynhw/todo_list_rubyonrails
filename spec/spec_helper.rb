bf71e885070ba497fd79bbf06c833590e80065f1
cynhw <cynhowong@gmail.com>
2016-01-03 03:18:45 -0800

    add length and presence validation to todo list title

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,6 +21,7 @@ RSpec.configure do |config|
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
   config.expect_with :rspec do |expectations|
+
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
     # defined using `chain`, e.g.:
@@ -29,6 +30,7 @@ RSpec.configure do |config|
     # ...rather than:
     #     # => "be bigger than 2"
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+    
   end
 
   # rspec-mocks config goes here. You can use an alternate test double
